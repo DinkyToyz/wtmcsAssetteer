b5d3805e3b0c09623d2a768c528638c46d47e0ab
Jonas <jonas@truls.org>
2017-07-29 23:35:50 +0200

    Empty reporter.

---
+++ wtmcsAsseteer/Reporter/AssetReporter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatThe.Mods.CitiesSkylines.Asseteer.Reporter
+{
+    internal class AssetReporter
+    {
+    }
+}
